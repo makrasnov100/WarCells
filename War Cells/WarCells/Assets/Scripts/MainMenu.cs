a10487d565a61310369dceac4261d4b8b7a4fabb
makrasnov100 <makrasnov100@gmail.com>
2019-01-21 16:11:00 -0800

    commit the stuff

---
+++ War Cells/WarCells/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public InputField inPlayers;
+
+    public void OnPlayBtn()
+    {
+        Object.DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene("GameScene");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "GameScene")
+        {
+            GameObject pm = GameObject.FindGameObjectWithTag("playerManager");
+            pm.GetComponent<PlayerManager>().SetHumanPlayers(int.Parse(inPlayers.text));
+            Destroy(gameObject);
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+
+
+
+}
+
